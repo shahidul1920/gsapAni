6d4964acc98806272ae21ec9344da3afb3abc098
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-05-05 20:15:16 +0600

    text animation

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const heading = document.querySelector("h1").textContent;
+const mainHeading = document.querySelector("h1")
 
+const splited = heading.split('');
+let clutter = ''
+
+splited.forEach((e)=>{
+    clutter += `<span>${e}</span>`
+})
+mainHeading.innerHTML = clutter
+
+gsap.from('h1 span',{
+    opacity: 0,
+    y: -100,
+    stagger: 0.1,
+    color: 'red'
+})
